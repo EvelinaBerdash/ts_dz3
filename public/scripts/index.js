5144c46f366ad1e90169c9eefcef2402b5431bd6
Eva <skyicelands@gmail.com>
2023-02-07 15:03:45 +0300

    dz3 ready

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,24 +1,28 @@
 import { renderSearchFormBlock } from "./search-form.js";
-import { renderSearchStubBlock } from "./search-results.js";
-import { renderUserBlock } from "./user.js";
-import { renderToast } from "./lib.js";
+import { renderSearchResultsBlock, renderSearchStubBlock } from "./search-results.js";
+import { getFavoritesAmount, renderUserBlock } from "./user.js";
+// import { renderToast } from "./lib.js"
 if (typeof window !== undefined && window && window.addEventListener) {
     window.addEventListener("DOMContentLoaded", () => {
         const today = new Date();
         const dateIn = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
         const dateOut = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 3);
-        renderUserBlock("Eva Berdash", "/img/avatar.png", 3);
+        renderUserBlock("Eva Berdash", "/img/avatar.png", getFavoritesAmount());
         renderSearchFormBlock(dateIn, dateOut);
         renderSearchStubBlock();
-        renderToast({
-            text: "Это пример уведомления. Используйте его при необходимости",
-            type: "success",
-        }, {
-            name: "Понял",
-            handler: () => {
-                console.log("Уведомление закрыто");
-            },
-        });
+        // renderToast(
+        //   {
+        //     text: "Это пример уведомления. Используйте его при необходимости",
+        //     type: "success",
+        //   },
+        //   {
+        //     name: "Понял",
+        //     handler: () => {
+        //       console.log("Уведомление закрыто");
+        //     },
+        //   }
+        // );
+        renderSearchResultsBlock();
     });
 }
 else {
